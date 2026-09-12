c4f09ead25f22b3f803489cccd272ed79b622fe8
prajwalsharmaa <prajwalst1213@gmail.com>
2026-09-12 22:49:45 +0545

    data validation

--- src/main/java/com/prajwal/jpa_demo/student/StudentController.java
+++ src/main/java/com/prajwal/jpa_demo/student/StudentController.java
@@ -1,8 +1,13 @@
 package com.prajwal.jpa_demo.student;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,7 +25,7 @@ public class StudentController {
 
     @PostMapping("/student")
     public StudentResponseDto saveStudent(
-            @RequestBody StudentDto dto
+            @Valid @RequestBody StudentDto dto
     ){
         return this.studentService.saveStudent(dto);
     }
@@ -49,5 +54,17 @@ public class StudentController {
     ){
         this.studentService.delete(id);
     }
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exp
+    ){
+        var errors = new HashMap<String,String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error ->{
+                    var fieldName = ((FieldError)error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName,errorMessage);
+                });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/prajwal/jpa_demo/student/StudentDto.java
+++ src/main/java/com/prajwal/jpa_demo/student/StudentDto.java
@@ -1,7 +1,11 @@
 package com.prajwal.jpa_demo.student;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentDto(
+        @NotEmpty
         String firstName,
+        @NotEmpty
         String lastName,
         String email,
         Integer schoolId
