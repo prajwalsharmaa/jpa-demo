fb743d3072e4b9b7772ea627873506fb5af1ec9c
prajwalsharmaa <prajwalst1213@gmail.com>
2026-09-15 22:55:00 +0545

    school mapper testing

--- src/test/java/com/prajwal/jpa_demo/student/StudentMapperTest.java
+++ src/test/java/com/prajwal/jpa_demo/student/StudentMapperTest.java
@@ -5,32 +5,22 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentMapperTest {
-    @BeforeAll
-    static void beforeAll() {
-        System.out.println("Inside beforeAll method with @BeforeAll annotation");
-    }
+    private StudentMapper mapper;
 
     @BeforeEach
     void setUp() {
-        System.out.println("Inside setup method with @BeforeEach annotation");
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.out.println("Inside tearDown method with @AfterEach annotation");
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("Inside afterAll method with @AfterAll annotation");
+        mapper = new StudentMapper();
     }
 
     @Test
-    public void testMethod1(){
-        System.out.println("My first test method");
-    }
-    @Test
-    public void testMethod2(){
-        System.out.println("My Second test method");
+    public void shouldMapStudentDtoToStudent(){
+        StudentDto dto = new StudentDto("John","Doe","john@gmail.com",1);
+        Student student = mapper.toStudent(dto);
+
+        assertEquals(dto.firstName(),student.getFirstname());
+        assertEquals(dto.lastName(),student.getLastname());
+        assertEquals(dto.email(),student.getEmail());
+        assertNotNull(student.getSchool());
+        assertEquals(dto.schoolId(),student.getSchool().getId());
     }
 }
